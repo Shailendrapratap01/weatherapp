cb3df1d12ef5991236e9e4b4acb7ee6895ac277f
shailendra01 <shailendrapratapsingh@innotechteam.in>
2024-04-11 19:18:53 +0530

    ui and approach changed

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("input");
 const weatherIcon = document.getElementById("weather-icon");
-const weatherInfo = document.getElementById("weather-info");
+const weatherCard = document.getElementById("weather-info");
 const temperature = document.getElementById("temperature");
 const weatherDesc = document.getElementById("weather-desc");
 const cityName = document.getElementById("location");
 const displayError = document.getElementById("not-found");
 const loader = document.getElementById("loader")
 const parentContainer = document.getElementById("main-container")
+
 
 const url = "https://api.openweathermap.org/data/2.5/weather?units=metric" ;
 const apiKey = "82005d27a116c2880c8f0fcb866998a0" ;
@@ -19,30 +20,59 @@ window.addEventListener("load", async () => {
 });
 
 const gotLocation = async (location)=>{
-    lat = location.coords.latitude;
-    lon = location.coords.longitude;
-    console.log(lat, lon);
-    city =  await getCurrentLocationWeather();
-    getWeather(city);
+    const lat = location.coords.latitude;
+    const lon = location.coords.longitude;
+    // console.log(lat, lon);
+    // city =  await getCurrentLocationWeather();
+    getWeather(lat, lon);
 }
 
 const failedToGetLocation = ()=>{
     console.log("there was some issue");
+    displayError.style.display = "flex";
+    loader.style.display = "none";
 }
 
-const getCurrentLocationWeather = async ()=>{
-    const res = await fetch(url + `&lat=${lat}` + `&lon=${lon}` + `&appid=${apiKey}`);
-    let data = await res.json();
-    console.log(data.name)
-    return data.name;
-}
-
-let getWeather = async (city)=>{
+// const getCurrentLocationWeather = async ()=>{
+//     const res = await fetch(url + `&q=${city}` + `&appid=${apiKey}`);
+//     let data = await res.json();
+//     console.log(data.name)
+//     return data.name;
+// }
+const getCoordinates = async ()=>{
+    const url = "http://api.openweathermap.org/geo/1.0/direct?limit=1";
     const res = await fetch(url + `&q=${city}` +`&appid=${apiKey}`);
+
     let data = await res.json();
+    if(data.length > 0){
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        return {lat, lon};
+    }
+    else{
+        weatherCard.style.display = "none";
+        displayError.style.display = "flex";
+
+    }
+}
+
+searchBtn.addEventListener("click", async ()=>{
+    city = searchInput.value;
+    searchInput.value = "";
+    const {lat, lon} = await getCoordinates(city);
+    getWeather(lat, lon);
+})
+
+let getWeather = async (lat, lon)=>{
+    loader.style.display = "block";
+    weatherCard.style.display = "none";
+    const res = await fetch(url + `&lat=${lat}` + `&lon=${lon}` +`&appid=${apiKey}`);
+    let data = await res.json();
+    loader.style.display = "block";
+    weatherCard.style.display = "flex";
 
     if(res.status == 200){
-        weatherInfo.style.display = "flex";
+        weatherCard.style.display = "flex";
         displayError.style.display = "none";
         loader.style.display = "none";
 
@@ -57,18 +87,14 @@ let getWeather = async (city)=>{
         const imgId = data.weather[0].description;
         const imgName = imgId.split(" ").join("_")
         parentContainer.style.backgroundImage = `url(./images/${imgName}.jpg)`;
+        console.log("2")
         
     }
     else{
-        weatherInfo.style.display = "none";
+        weatherCard.style.display = "none";
         displayError.style.display = "flex";
+        loader.style.display = "block";
     }
     console.log(data);
     console.log(res.status);
 }
-
-searchBtn.addEventListener("click", ()=>{
-    city = searchInput.value;
-    console.log(searchInput.value)
-    getWeather(city);
-})
