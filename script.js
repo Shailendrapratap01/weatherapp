0133bcaff5a67955cad0c001e2a181669dd0a3b8
shailendra01 <shailendrapratapsingh@innotechteam.in>
2024-04-12 19:07:47 +0530

    5day forecast data add

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const cityName = document.getElementById("location");
 const error = document.getElementById("not-found");
 const loader = document.getElementById("loader");
 const bodyContainer = document.getElementById("main-container");
+const weatherForecastDiv = document.getElementById("weather-cards-div");
+
 
 const apiKey = "82005d27a116c2880c8f0fcb866998a0";
 let city;
@@ -64,6 +66,7 @@ searchBtn.addEventListener("click", async () => {
     const lon = getCoordinatesData?.lon;
     if (lat && lon) {
       getWeather(lat, lon);
+	  getForcastWeather(lat, lon);
     }
   }
 });
@@ -92,3 +95,42 @@ const getWeather = async (lat, lon) => {
     bodyContainer.style.backgroundImage = "url(./images/default.png)";
   }
 };
+
+const createForecastDiv = (p1,p2,p3,p4) => {
+    return `<div class="weather-forecast-div">
+                <div class="weather-forecast-date">
+                    <p>${p1}</p>
+                </div>
+                <div class="weather-forecast-descandtemp">
+                    <div class="weather-forecast-imgandtemp">
+                        <div class="weather-forecast-img">
+                            <img src="${p2}" alt="">
+                        </div>
+                        <div class="weather-forecast-temp">
+                            <p>${p3}</p>
+                        </div>
+                    </div>
+                    <div class="weather-forecast-desc">
+                        <p>${p4}</p>
+                    </div>
+                </div>
+            </div>`
+}
+
+const getForcastWeather = async (lat, lon) => {
+    // console.log(apiKey)
+    let card;
+    let n=8;
+    const { res, data } = await fetchData(`https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`);
+    for(let i=0; i<4; i++){
+        console.log(data)
+        const temp = data.list[n].main.temp;
+        const date = data.list[n].dt;
+        const forecastIcon = data.list[n].weather[0].icon;
+        const forecastDesc = data.list[n].weather[0].description;
+        n+=8;
+        card += createForecastDiv(temp, date, forecastIcon, forecastDesc);
+    }
+    console.log(card)
+    weatherForecastDiv.innerHTML=card;
+}
